43f1a13326dc85b7f0dbf3c0beb342953cf85f6d
Ryna Truong <rynatruong@outlook.com>
2020-07-15 18:19:20 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,8 @@
+class MP3Importer
+  
+  attr_accessor :file
+  
+  def initialize(file)
+    @file = file
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+  
+  @@all = []
+  
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+end
