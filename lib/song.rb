7a0b0b166c7be67e15f7a8b915d2194afd3cdf24
Ryna Truong <rynatruong@outlook.com>
2020-07-15 18:24:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,10 +14,10 @@ class Song
   end
   
  def self.new_by_filename(file)
-      song_info = file.chomp(".mp3").split(" - ")
-      song = Song.new(song_info[1])
-      song.artist_name = song_info[0]
-      song
+    song_file = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_file[1])
+    song.artist_name = song_file[0]
+    song
     end
   
 end
